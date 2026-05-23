50db8e9f112eebed5f2f435d361d86b5b378a60f
giomanzo96 <giomanzo96@gmail.com>
2026-05-23 15:50:57 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config(); // carica le variabili d'ambiente dal file .env
+
+const express = require('express');
+const path = require('path'); // importa il modulo path per gestire i percorsi dei file
+const ejsMate = require('ejs-mate');
+const { Pool } = require('pg'); // importa il modulo pg per interagire con il database PostgreSQL. Pool è una classe che gestisce un pool di connessioni al database, consentendo di eseguire query in modo efficiente e scalabile.
+
+const pool = new Pool({  // crea un nuovo pool di connessioni al database utilizzando le variabili d'ambiente per la configurazione
+    connectionString: process.env.DATABASE_URL
+});const app = express();
+
+
+app.engine('ejs', ejsMate); // imposta ejs-mate come motore di rendering per i file .ejs
+app.set('view engine', 'ejs'); // imposta ejs come motore di rendering predefinito
+app.set('views', path.join(__dirname, 'views')); // imposta la cartella 'views' come directory per i file di visualizzazione
+
+
+app.use(express.urlencoded({ extended: true })); // middleware per parsare i dati del corpo delle richieste POST
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/operations/index', (req, res) => {
+    res.render('operations/index');
+});
+
+app.get('/operations/visita', (req, res) => {
+    res.render('operations/visita');
+});
+
+app.get('/operations/cliente', (req, res) => {
+    res.render('operations/cliente');
+});
+
+app.post('/operations/cliente', async (req, res) => {
+    const { nome, cognome, telefono, email, note } = req.body;
+
+    const sql = `
+        INSERT INTO clienti_acquisizione 
+        (nome, cognome, telefono, email, note)
+        VALUES ($1, $2, $3, $4, $5)
+    `;
+
+    const values = [nome, cognome, telefono, email, note];
+
+    await pool.query(sql, values);
+
+    res.redirect('/');
+});
+
+app.get('/fine_demo', (req, res) => {
+    res.render('operations/fine_demo');
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server in ascolto sulla porta ${process.env.PORT}`);
+});
